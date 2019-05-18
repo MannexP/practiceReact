6ef62192ff98d4a5b70879c07da4867c0045dfca
Mannex Pimentel <mannex247@gmail.com>
2019-05-18 17:50:22 -0400

    established state, constructor and super

--- src/index.js
+++ src/index.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-    (position) => console.log(position),
-    (err) => console.log(err)
-    );
 
+class App extends React.Component {
+ constructor(props) {
+    super(props);
 
+    this.state = { lat: null};
+ }
 
-    return <div>Hi There!</div>;
-};
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            (position) => console.log(position),
+            (err) => console.log(err)
+            );
+        
+        
+        return <div>Latitude: </div>;
+    }
+}
 
 ReactDOM.render(<App/>, document.querySelector('#root'))
+
