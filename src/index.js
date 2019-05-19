4105cafb25b95debafbc43df037933c699cc0858
Mannex Pimentel <mannex247@gmail.com>
2019-05-19 00:10:29 -0400

    setState working

--- src/index.js
+++ src/index.js
@@ -1,24 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+
+
+// class App extends React.Component {
+//     constructor(props) {
+//         super(props);
+//         this.state = { lat: null };
+
+//         window.navigator.geolocation.getCurrentPosition(
+//             (position) => console.log(position),
+           
+//             (err) => console.log(err)
+//           );
+        
+    
+//     }
+    
+   
+//     render() {
+      
+//         return <div>Latitude: </div>;
+//     }
+// }
 
 
 class App extends React.Component {
- constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
+        this.state = { lat: null };
+        
 
-    this.state = { lat: null};
- }
-
-    render() {
         window.navigator.geolocation.getCurrentPosition(
-            (position) => console.log(position),
+            (position) => {
+                this.setState({ lat: position.coords.latitude });
+            },
+           
             (err) => console.log(err)
-            );
+          );
         
-        
-        return <div>Latitude: </div>;
+    
+    }
+    
+   
+    render() {
+      
+        return <div>Latitude:{this.state.lat}</div>;
     }
 }
 
-ReactDOM.render(<App/>, document.querySelector('#root'))
+
+
+ReactDOM.render(<App />, document.querySelector("#root"));
+
 
