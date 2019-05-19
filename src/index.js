1fd63b1f6aacf236a8e35f52ebc4f68b899920c1
Mannex Pimentel <mannex247@gmail.com>
2019-05-19 02:59:37 -0400

    refactor

--- src/index.js
+++ src/index.js
@@ -1,44 +1,44 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
-
-
-
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
 class App extends React.Component {
-    // constructor(props) {
-    //     super(props);
-    //     this.state = { lat: null, errorMessage:'' }; 
-    // }
-        // -------^^^^^SAME THING^^^^^^------------
-    state = { lat: null, errorMessage:''};
+  // constructor(props) {
+  //     super(props);
+  //     this.state = { lat: null, errorMessage:'' };
+  // }
+  // -------^^^^^SAME THING^^^^^^------------
+  state = { lat: null, errorMessage: "" };
 
-    //  --------------------------------------------
+  //  --------------------------------------------
 
-    componentDidMount(){
-        window.navigator.geolocation.getCurrentPosition(
-            (position) => this.setState({ lat: position.coords.latitude, errorMessage:'' }), 
-            (err) => this.setState({ errorMessage:err.message })
-          );
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position =>
+        this.setState({ lat: position.coords.latitude, errorMessage: "" }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div> Error: {this.state.errorMessage}</div>;
     }
-    
 
-
-    render() {
-        if(this.state.errorMessage && !this.state.lat) {
-            return <div> Error: {this.state.errorMessage}</div>;
-        }
-
-        if(!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-        return <div><Spinner message="Please accept location rquest" /></div>;
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
     }
+    return (
+      <div>
+        <Spinner message="Please accept location rquest" />
+      </div>
+    );
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
 }
 
-
-
 ReactDOM.render(<App />, document.querySelector("#root"));
-
-
